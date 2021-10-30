128d1168f281c3c63a13f06266d1fb47e7e730ce
Iskandarkenz <miskandarrasyid11@gmail.com>
2021-10-30 13:52:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/yuknabung_group1/Activity_UbahProfilEdit.java
@@ -0,0 +1,22 @@
+package com.example.yuknabung_group1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_UbahProfilEdit extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_profil_edit);
+
+    }
+    public void pilihFoto(){
+
+
+    }
+
+
+}
