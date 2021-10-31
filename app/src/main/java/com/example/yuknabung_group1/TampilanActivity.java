078fbb5702c33c513c3da7cd7e99fce9d67c42ab
Iskandarkenz <miskandarrasyid11@gmail.com>
2021-10-31 08:16:02 +0700

    ubah nama xml&java

--- app/src/main/java/com/example/yuknabung_group1/TampilanActivity.java
+++ app/src/main/java/com/example/yuknabung_group1/TampilanActivity.java
@@ -9,6 +9,6 @@ public class TampilanActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tampilan);
+        setContentView(R.layout.activity_nominal_terkumpul);
     }
 }
--- app/src/main/java/com/example/yuknabung_group1/TampilanEditGitarActivity.java
+++ app/src/main/java/com/example/yuknabung_group1/TampilanEditGitarActivity.java
@@ -9,6 +9,6 @@ public class TampilanEditGitarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tampilan_edit_gitar);
+        setContentView(R.layout.activity_edit_target_gitar);
     }
 }
